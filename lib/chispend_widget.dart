7fcced9724cf5a2d8b587d162af77518f66e6792
Somto-Facade <bonaventureezenwaka@gmail.com>
2022-10-05 14:00:13 +0100

    Refactored events logging to be optional

--- lib/chispend_widget.dart
+++ lib/chispend_widget.dart
@@ -1,11 +1,3 @@
-// library chispend_widget;
-//
-// /// A Calculator.
-// class Calculator {
-//   /// Returns [value] plus 1.
-//   int addOne(int value) => value + 1;
-// }
-
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
@@ -14,22 +6,20 @@ import 'package:webview_flutter/webview_flutter.dart';
 enum ChiSpendTheme { light, dark, moonlight, royal }
 
 class ChiSpendWidget extends StatefulWidget {
-
   /// Invoked when a javascript message is returned from the ChiSpendChannel.
   final void Function(String)? onMessageReceived;
-
 
   /// Max amount users can spend in USD.
   final double maxAmountInUSD;
 
-
   /// Primary colour used. Defaults to Purple.
   final Color primaryColor;
-
 
   /// The widget's theme. Defaults to light mode.
   final ChiSpendTheme chiSpendTheme;
 
+  /// DebugPrint events as they occur. Defaults to true.
+  final bool logEvent;
 
   /// Creates a new ChiSpend widget.
   ///
@@ -41,7 +31,8 @@ class ChiSpendWidget extends StatefulWidget {
       this.onMessageReceived,
       this.chiSpendTheme = ChiSpendTheme.light,
       required this.maxAmountInUSD,
-      this.primaryColor = Colors.purple})
+      this.primaryColor = Colors.purple,
+      this.logEvent = true})
       : super(key: key);
 
   @override
@@ -51,6 +42,7 @@ class ChiSpendWidget extends StatefulWidget {
 class _ChiSpendWidgetState extends State<ChiSpendWidget> {
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
+  final String chispendHost = 'https://chispend.com';
 
   @override
   void initState() {
@@ -59,15 +51,17 @@ class _ChiSpendWidgetState extends State<ChiSpendWidget> {
       WebView.platform = SurfaceAndroidWebView();
     }
   }
-/*
-*/
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
           body: WebView(
-        initialUrl:
-            'https://chispend.com/?cSContext=mobile&primaryColor=${widget.primaryColor.value.toRadixString(16).substring(2)}&xAppStyle=${widget.chiSpendTheme.name}&maxAmountInUSD=${widget.maxAmountInUSD}',
+        initialUrl: chispendHost +
+            '/?cSContext=mobile' +
+            '&primaryColor=${widget.primaryColor.value.toRadixString(16).substring(2)}' +
+            '&xAppStyle=${widget.chiSpendTheme.name}' +
+            '&maxAmountInUSD=${widget.maxAmountInUSD}',
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (WebViewController webViewController) {
           _controller.complete(webViewController);
@@ -79,18 +73,24 @@ class _ChiSpendWidgetState extends State<ChiSpendWidget> {
           JavascriptChannel(
             name: 'ChiSpendChannel',
             onMessageReceived: (message) async {
-              debugPrint('Javascript: ${message.message}');
-              if(widget.onMessageReceived != null){
+              if (widget.logEvent) {
+                debugPrint('Javascript: ${message.message}');
+              }
+              if (widget.onMessageReceived != null) {
                 widget.onMessageReceived!(message.message);
               }
             },
           ),
         },
         onPageStarted: (String url) {
-          debugPrint('Page started loading: $url');
+          if (widget.logEvent) {
+            debugPrint('Page started loading: $url');
+          }
         },
         onPageFinished: (String url) {
-          debugPrint('Page finished loading: $url');
+          if (widget.logEvent) {
+            debugPrint('Page finished loading: $url');
+          }
         },
         gestureNavigationEnabled: true,
         backgroundColor: const Color(0x00000000),
